337db0af59bf332653f9872656f8646bec883a95
Dmitriy Kaygorodov <dbkai@ya.ru>
2022-03-19 16:45:06 +0500

    feat: добавить плавное открытие и закрытие попапов

--- scripts/index.js
+++ scripts/index.js
@@ -1,36 +1,29 @@
 const initialCards = [
   {
     name: 'Карачаевск',
-    link: '../images/kirill-pershin-1088404-unsplash.jpg',
-    description: 'Карачаевск'
+    link: '../images/kirill-pershin-1088404-unsplash.jpg'
   },
   {
     name: 'Гора Эльбрус',
-    link: '../images/kirill-pershin-1404681-unsplash.jpg',
-    description: 'Гора Эльбрус'
+    link: '../images/kirill-pershin-1404681-unsplash.jpg'
   },
   {
     name: 'Домбай',
-    link: '../images/kirill-pershin-1556355-unsplash.jpg',
-    description: 'Домбай'
+    link: '../images/kirill-pershin-1556355-unsplash.jpg'
   },
   {
     name: 'Гора Эльбрус',
-    link: '../images/kirill-pershin-1404681-unsplash.jpg',
-    description: 'Гора Эльбрус'
+    link: '../images/kirill-pershin-1404681-unsplash.jpg'
   },
   {
     name: 'Домбай',
-    link: '../images/kirill-pershin-1556355-unsplash.jpg',
-    description: 'Домбай'
+    link: '../images/kirill-pershin-1556355-unsplash.jpg'
   },
   {
     name: 'Карачаево-Черкессия',
-    link: '../images/kirill-pershin-1088404-unsplash.jpg',
-    description: 'Карачаево-Черкессия'
+    link: '../images/kirill-pershin-1088404-unsplash.jpg'
   }
 ];
-
 
 /*  Изменение профиля  */
 
@@ -39,7 +32,7 @@ const initialCards = [
 const profileEditButton = document.querySelector('.profile__edit');
 
 // Находим форму изменения профиля в DOM
-const popupProfile = document.querySelector('.popup_profile');
+const popupProfile = document.querySelector('.popup_type_profile');
 
 // Находим поля формы изменения данных профиля в DOM
 const profileNameInput = popupProfile.querySelector('.popup__item_profile_name');
@@ -49,14 +42,21 @@ const profileJobInput = popupProfile.querySelector('.popup__item_profile_job');
 const profileName = document.querySelector('.profile__name');
 const profileJob = document.querySelector('.profile__job');
 
-// Прикрепляем обработчик к кнопке изменения профиля
-profileEditButton.addEventListener('click', (event) => {
-  profileNameInput.value = profileName.textContent;
-  profileJobInput.value = profileJob.textContent;
-  openPopup(popupProfile);
-  // Вешаем событие click на кнопку закрытия popup
-  popupProfile.querySelector('.popup__close').addEventListener('click', closePopup);
-});
+// Находим блок с карточками
+const cardContainer = document.querySelector('.cards');
+
+// Находим кнопку добавить карточку в DOM
+const cardAddButton = document.querySelector('.profile__card-add');
+
+// Находим форму добавления карточки в DOM
+const popupCard = document.querySelector('.popup_type_card');
+
+// Находим поля формы карточки в DOM
+const cardNameInput = popupCard.querySelector('.popup__item_card_name');
+const cardLinkInput = popupCard.querySelector('.popup__item_card_link');
+
+// Находим форму добавления просмотра полного изображения в DOM
+const imageView = document.querySelector('.popup_type_image-view');
 
 // Обработчик «отправки» формы, хотя пока она никуда отправляться не будет
 function profileFormSubmitHandler(event) {
@@ -66,9 +66,6 @@ function profileFormSubmitHandler(event) {
   profileJob.textContent = profileJobInput.value;
   closePopup(event);
 }
-
-// Прикрепляем обработчик к форме: он будет следить за событием “submit” - «отправка»
-popupProfile.addEventListener('submit', profileFormSubmitHandler);
 
 // Функция добавляет модификатор opened
 function openPopup(popup) {
@@ -80,27 +77,6 @@ function closePopup(event) {
   event.currentTarget.closest('.popup').classList.remove('popup_opened');
 }
 
-
-/*  Добавление карточки  */
-
-
-// Находим кнопку добавить карточку в DOM
-const cardAddButton = document.querySelector('.profile__card-add');
-
-// Находим форму добавления карточки в DOM
-const popupCard = document.querySelector('.popup_card');
-
-// Находим поля формы карточки в DOM
-const cardNameInput = popupCard.querySelector('.popup__item_card_name');
-const cardLinkInput = popupCard.querySelector('.popup__item_card_link');
-
-// Прикрепляем обработчик к кнопке добавить карточку
-cardAddButton.addEventListener('click', () => {
-  openPopup(popupCard);
-  // Вешаем событие click на кнопку закрытия popup
-  popupCard.querySelector('.popup__close').addEventListener('click', closePopup);
-});
-
 // Обработчик «отправки» формы, хотя пока она никуда отправляться не будет
 function cardFormSubmitHandler(event) {
   event.preventDefault(); // Эта строчка отменяет стандартную отправку формы.
@@ -109,16 +85,6 @@ function cardFormSubmitHandler(event) {
   cardLinkInput.value = '';
   closePopup(event);
 }
-
-// Прикрепляем обработчик к форме: он будет следить за событием “submit” - «отправка»
-popupCard.addEventListener('submit', cardFormSubmitHandler);
-
-
-/*  Заполнение контейнера карточками  */
-
-
-// Находим блок с карточками
-const cardContainer = document.querySelector('.cards');
 
 // Функция добавляет блок с карточкой
 function addCard(cardTitle, imageSrc) {
@@ -131,6 +97,7 @@ function addCard(cardTitle, imageSrc) {
   cardElement.querySelector('.card__image').alt = cardTitle;
   cardElement.querySelector('.card__like').addEventListener('click', likeCard);
   cardElement.querySelector('.card__remove').addEventListener('click', removeCard);
+  cardElement.querySelector('.card__image').addEventListener('click', () => viewCard(cardTitle, imageSrc));
   // Добавляем в начало контейнера с карточками
   cardContainer.prepend(cardElement);
 }
@@ -145,10 +112,14 @@ function likeCard(event) {
   event.target.classList.toggle('card__like_active');
 }
 
-// Открытие картинки в попапе
-// При вызове функции обратного вызова слушателя, который сработает при нажатии на картинку,
-// передайте в нее (в функцию) данные самой карточки. Внутри этой функции вам надо данные этой карточки
-// положить в нужные места в разметке вашего попапа. После этого открыть попап.
+// Функция просмотра и редактирования карточки
+function viewCard(cardTitle, imageSrc) {
+  imageView.querySelector('.popup__image').src = imageSrc;
+  imageView.querySelector('.popup__caption').textContent = cardTitle;
+  // Вешаем событие click на кнопку закрытия popup
+  imageView.querySelector('.popup__close').addEventListener('click', closePopup);
+  openPopup(imageView);
+}
 
 // Функция читает массив и выводит карточки в DOM
 function renderCards() {
@@ -158,6 +129,27 @@ function renderCards() {
 }
 
 renderCards();
+
+// Прикрепляем обработчик к кнопке изменения профиля
+profileEditButton.addEventListener('click', (event) => {
+  profileNameInput.value = profileName.textContent;
+  profileJobInput.value = profileJob.textContent;
+  openPopup(popupProfile);
+  // Вешаем событие click на кнопку закрытия popup
+  popupProfile.querySelector('.popup__close').addEventListener('click', closePopup);
+});
+
+// Прикрепляем обработчик к кнопке добавить карточку
+cardAddButton.addEventListener('click', () => {
+  openPopup(popupCard);
+  // Вешаем событие click на кнопку закрытия popup
+  popupCard.querySelector('.popup__close').addEventListener('click', closePopup);
+});
+
+// Прикрепляем обработчик к форме: он будет следить за событием “submit” - «отправка»
+popupProfile.addEventListener('submit', profileFormSubmitHandler);
+// Прикрепляем обработчик к форме: он будет следить за событием “submit” - «отправка»
+popupCard.addEventListener('submit', cardFormSubmitHandler);
 
 // Функция определяет совершен ли клик по области popup
 // function withinPopup(event) {
