0560b1350cf22ae69aa2ff99555c4fef62531383
Dmitriy Kaygorodov <dbkai@ya.ru>
2022-03-19 21:13:34 +0500

    fix: устранить замечания ревьюера №1

---
+++ scripts/cards.js
@@ -0,0 +1,26 @@
+const initialCards = [
+  {
+    name: 'Карачаевск',
+    link: './images/kirill-pershin-1088404-unsplash.jpg'
+  },
+  {
+    name: 'Гора Эльбрус',
+    link: './images/kirill-pershin-1404681-unsplash.jpg'
+  },
+  {
+    name: 'Домбай',
+    link: './images/kirill-pershin-1556355-unsplash.jpg'
+  },
+  {
+    name: 'Гора Эльбрус',
+    link: './images/kirill-pershin-1404681-unsplash.jpg'
+  },
+  {
+    name: 'Домбай',
+    link: './images/kirill-pershin-1556355-unsplash.jpg'
+  },
+  {
+    name: 'Карачаево-Черкессия',
+    link: './images/kirill-pershin-1088404-unsplash.jpg'
+  }
+];
--- scripts/index.js
+++ scripts/index.js
@@ -1,38 +1,10 @@
-const initialCards = [
-  {
-    name: 'Карачаевск',
-    link: './images/kirill-pershin-1088404-unsplash.jpg'
-  },
-  {
-    name: 'Гора Эльбрус',
-    link: './images/kirill-pershin-1404681-unsplash.jpg'
-  },
-  {
-    name: 'Домбай',
-    link: './images/kirill-pershin-1556355-unsplash.jpg'
-  },
-  {
-    name: 'Гора Эльбрус',
-    link: './images/kirill-pershin-1404681-unsplash.jpg'
-  },
-  {
-    name: 'Домбай',
-    link: './images/kirill-pershin-1556355-unsplash.jpg'
-  },
-  {
-    name: 'Карачаево-Черкессия',
-    link: './images/kirill-pershin-1088404-unsplash.jpg'
-  }
-];
-
-/*  Изменение профиля  */
-
-
 // Находим кнопку изменить данные профиля в DOM
 const profileEditButton = document.querySelector('.profile__edit');
 
 // Находим форму изменения профиля в DOM
 const popupProfile = document.querySelector('.popup_type_profile');
+
+const popupProfileClose = popupProfile.querySelector('.popup__close');
 
 // Находим поля формы изменения данных профиля в DOM
 const profileNameInput = popupProfile.querySelector('.popup__item_profile_name');
@@ -48,6 +20,9 @@ const cardContainer = document.querySelector('.cards');
 // Находим кнопку добавить карточку в DOM
 const cardAddButton = document.querySelector('.profile__card-add');
 
+// Находим шаблон карточки
+const cardTemplate = document.querySelector('#card-template').content;
+
 // Находим форму добавления карточки в DOM
 const popupCard = document.querySelector('.popup_type_card');
 
@@ -58,13 +33,19 @@ const cardLinkInput = popupCard.querySelector('.popup__item_card_link');
 // Находим форму добавления просмотра полного изображения в DOM
 const imageView = document.querySelector('.popup_type_image-view');
 
+const cardImage = imageView.querySelector('.popup__image');
+const cardCaption = imageView.querySelector('.popup__caption');
+const popupImageViewClose = imageView.querySelector('.popup__close');
+
+const popupCardClose =  popupCard.querySelector('.popup__close');
+
 // Обработчик «отправки» формы, хотя пока она никуда отправляться не будет
 function profileFormSubmitHandler(event) {
   event.preventDefault(); // Эта строчка отменяет стандартную отправку формы.
   // Вставляем новые значения с помощью textContent
   profileName.textContent = profileNameInput.value;
   profileJob.textContent = profileJobInput.value;
-  closePopup(event);
+  closePopup(popupProfile);
 }
 
 // Функция добавляет модификатор opened
@@ -73,33 +54,37 @@ function openPopup(popup) {
 }
 
 // Функция удаляет модификатор opened
-function closePopup(event) {
-  event.currentTarget.closest('.popup').classList.remove('popup_opened');
+function closePopup(popup) {
+  popup.classList.remove('popup_opened');
 }
 
 // Обработчик «отправки» формы, хотя пока она никуда отправляться не будет
 function cardFormSubmitHandler(event) {
   event.preventDefault(); // Эта строчка отменяет стандартную отправку формы.
-  addCard(cardNameInput.value, cardLinkInput.value);
+  addCard(generateCard(cardNameInput.value, cardLinkInput.value));
   cardNameInput.value = '';
   cardLinkInput.value = '';
-  closePopup(event);
+  closePopup(popupCard);
 }
 
 // Функция добавляет блок с карточкой
-function addCard(cardTitle, imageSrc) {
-  const cardTemplate = document.querySelector('#card-template').content;
+function generateCard(cardTitle, imageSrc) {
   // Клонируем template
   const cardElement = cardTemplate.querySelector('.card').cloneNode(true);
+  const cardImage = cardElement.querySelector('.card__image');
   // Заполняем данными
   cardElement.querySelector('.card__title').textContent = cardTitle;
-  cardElement.querySelector('.card__image').src = imageSrc;
-  cardElement.querySelector('.card__image').alt = cardTitle;
+  cardImage.src = imageSrc;
+  cardImage.alt = cardTitle;
   cardElement.querySelector('.card__like').addEventListener('click', likeCard);
   cardElement.querySelector('.card__remove').addEventListener('click', removeCard);
-  cardElement.querySelector('.card__image').addEventListener('click', () => viewCard(cardTitle, imageSrc));
+  cardImage.addEventListener('click', () => viewCard(cardTitle, imageSrc));
+  return cardElement;
+}
+
+function addCard(generateCard) {
   // Добавляем в начало контейнера с карточками
-  cardContainer.prepend(cardElement);
+  cardContainer.prepend(generateCard);
 }
 
 // Функция удаления карточки
@@ -114,17 +99,16 @@ function likeCard(event) {
 
 // Функция просмотра и редактирования карточки
 function viewCard(cardTitle, imageSrc) {
-  imageView.querySelector('.popup__image').src = imageSrc;
-  imageView.querySelector('.popup__caption').textContent = cardTitle;
-  // Вешаем событие click на кнопку закрытия popup
-  imageView.querySelector('.popup__close').addEventListener('click', closePopup);
+  cardCaption.textContent = cardTitle;
+  cardImage.src = imageSrc;
+  cardImage.alt = cardTitle;
   openPopup(imageView);
 }
 
 // Функция читает массив и выводит карточки в DOM
 function renderCards() {
   initialCards.forEach((item) => {
-    addCard(item.name, item.link);
+    addCard(generateCard(item.name, item.link));
   });
 }
 
@@ -135,27 +119,20 @@ profileEditButton.addEventListener('click', (event) => {
   profileNameInput.value = profileName.textContent;
   profileJobInput.value = profileJob.textContent;
   openPopup(popupProfile);
-  // Вешаем событие click на кнопку закрытия popup
-  popupProfile.querySelector('.popup__close').addEventListener('click', closePopup);
 });
 
 // Прикрепляем обработчик к кнопке добавить карточку
 cardAddButton.addEventListener('click', () => {
   openPopup(popupCard);
-  // Вешаем событие click на кнопку закрытия popup
-  popupCard.querySelector('.popup__close').addEventListener('click', closePopup);
 });
 
 // Прикрепляем обработчик к форме: он будет следить за событием “submit” - «отправка»
 popupProfile.addEventListener('submit', profileFormSubmitHandler);
+// Вешаем событие click на кнопку закрытия popup
+popupProfileClose.addEventListener('click', () => closePopup(popupProfile));
 // Прикрепляем обработчик к форме: он будет следить за событием “submit” - «отправка»
 popupCard.addEventListener('submit', cardFormSubmitHandler);
-
-// Функция определяет совершен ли клик по области popup
-// function withinPopup(event) {
-//   if (event.target === event.currentTarget) {
-//     closePopup();
-//   }
-// }
-// Прикрепляем обработчик события click на popup
-// formElement.addEventListener('click', withinPopup);
+// Вешаем событие click на кнопку закрытия popup
+popupCardClose.addEventListener('click', () => closePopup(popupCard));
+// Вешаем событие click на кнопку закрытия popup
+popupImageViewClose.addEventListener('click', () => closePopup(imageView));
