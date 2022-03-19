b4ca8ffaab214f6b4a34cbdb91d28c2cb558f7d4
Dmitriy Kaygorodov <dbkai@ya.ru>
2022-03-19 18:16:24 +0500

    fix: изменить пути к изображениям

--- scripts/index.js
+++ scripts/index.js
@@ -1,27 +1,27 @@
 const initialCards = [
   {
     name: 'Карачаевск',
-    link: '../images/kirill-pershin-1088404-unsplash.jpg'
+    link: './images/kirill-pershin-1088404-unsplash.jpg'
   },
   {
     name: 'Гора Эльбрус',
-    link: '../images/kirill-pershin-1404681-unsplash.jpg'
+    link: './images/kirill-pershin-1404681-unsplash.jpg'
   },
   {
     name: 'Домбай',
-    link: '../images/kirill-pershin-1556355-unsplash.jpg'
+    link: './images/kirill-pershin-1556355-unsplash.jpg'
   },
   {
     name: 'Гора Эльбрус',
-    link: '../images/kirill-pershin-1404681-unsplash.jpg'
+    link: './images/kirill-pershin-1404681-unsplash.jpg'
   },
   {
     name: 'Домбай',
-    link: '../images/kirill-pershin-1556355-unsplash.jpg'
+    link: './images/kirill-pershin-1556355-unsplash.jpg'
   },
   {
     name: 'Карачаево-Черкессия',
-    link: '../images/kirill-pershin-1088404-unsplash.jpg'
+    link: './images/kirill-pershin-1088404-unsplash.jpg'
   }
 ];
 
